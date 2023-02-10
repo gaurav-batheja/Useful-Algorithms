79bd52965fed145be57e978e9f2eb68ed32962de
gaurav-batheja <123798033+gaurav-batheja@users.noreply.github.com>
2023-02-10 13:45:44 +0530

    Create matrixMultiplication.py

---
+++ matrixMultiplication.py
@@ -0,0 +1,43 @@
+def initialize_mat(dim):
+  c=[]
+  for i in range(dim):
+    c.append([])
+  for i in range(dim):
+    for j in range(dim):
+      c[i].append(0)
+  return(c)
+  
+  
+def dot_product(u,v):
+  dim=len(u)
+  sum=0
+  for i in range(dim):
+    sum=sum+(u[i]*v[i])
+  return(sum)
+
+
+def row(M,i):
+  l=[]
+  dim=len(M)
+  for k in range(dim):
+    l.append(M[i][k])
+  return l
+
+
+def column(M,j):
+  l=[]
+  dim=len(M)
+  for k in range(dim):
+    l.append(M[k][j])
+  return l
+M=[[1,2,3],[4,5,6],[7,8,9]]
+
+
+
+def mat_mul(A,B):
+  dim=len(A)
+  C=initialize_mat(dim)
+  for i in range(dim):
+    for j in range(dim):
+      C[i][j]=dot_product(row(A,i),column(B,j))
+  return(C)
