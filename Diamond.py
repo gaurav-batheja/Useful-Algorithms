21bc3a7eb27a02d985fa876974bc06723be6627d
gaurav-batheja <123798033+gaurav-batheja@users.noreply.github.com>
2023-02-13 17:39:14 +0530

    Update Diamond.py

--- Diamond.py
+++ Diamond.py
@@ -1,7 +1,9 @@
-n=int(input())
-L=[" "*(n-i)+"*"*(i)for i in range(1,n)]
+n=int(input())#min 3
+L=[" "*(n-i)+"* "*(i)for i in range(1,n)]
+print()
 for i in L:
-  print(i+i[-1::-1])
+  print(i)
+L.remove(L[-1])
 L.reverse()
 for i in L:
-  print(i+i[-1::-1])
+  print(i)
